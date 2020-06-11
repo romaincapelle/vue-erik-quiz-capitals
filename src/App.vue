ccba149c70d0ff5faa3cb3424bbd2f23b1c1d1b1
romaincapelle <romain.capelle@gmail.com>
2020-06-11 16:54:10 -0400

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="app" class="bg-indigo-900 min-h-screen ">
-    <div id="nav">
-      <Header />
-    </div>
-    <router-view />
+  <div id="app" class="bg-indigo-900 flex flex-col min-h-screen">
+    <Header />
+    <div class="flex-grow"><router-view /></div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="w-full text-center border-t border-grey-500 p-4 pin-b">
+    <a
+      href="https://romaincapelle.com"
+      class="text-sm text-gray-500 cursor-pointer underline hover:no-underline"
+      target="_blank"
+    >
+      By Romain Capelle
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
